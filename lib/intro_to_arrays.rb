e29ae1d1bd3de2fd0d77c03345c0c54ae84ff02a
courtney persons <personscourtney@gmail.com>
2018-09-21 04:00:18 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def  instantiate_new_array
+[]
+end
+
+
+def  array_with_two_elements
+  [1,2]
+end
+
+def first_element(taylor_swift)
+  taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift.last
+end
+
+
+def first_element_with_array_methods(south_east_asia)
+south_east_asia.shift()
+end
+
+def last_element_with_array_methods(south_east_asia)
+south_east_asia.pop()
+end
+
+def length_of_array(programming_languages)
+programming_languages.length()
+end
