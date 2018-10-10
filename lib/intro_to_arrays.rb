2e6e954bf8aa6beb1aae0925e328787006ecf0b7
grant smith <grantsmith1256@gmail.com>
2018-10-10 05:07:27 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+ @my_new_array=[]
+end
+
+def array_with_two_elements
+  @my_two_array=[]
+  @my_two_array << "happy"<< "now"
+end
+
+def first_element(index)
+  @taylor_swift.first
+end
+
+def third_element(index)
+  @taylor_swift[2]
+end
+
+def last_element(index)
+  @taylor_swift.last
+end
+
+def first_element_with_array_methods(index)
+  @south_east_asia.first
+end
+
+def last_element_with_array_methods(index)
+  @south_east_asia.last
+end
+
+def length_of_array(length)
+  @programming_languages.count
+end
