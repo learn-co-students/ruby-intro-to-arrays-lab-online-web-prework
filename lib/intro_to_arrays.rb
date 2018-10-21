cf8dfad8f4022afb110e0b2f71592e59117203f6
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-21 18:27:40 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+@my_new_array = []
+end
+
+def array_with_two_elements 
+  @my_new_array = ["one", "two"]
+end
+
+def first_element(array)
+  @my_new_array = "Welcome to New York"
+end
+
+def third_element(array)
+  @my_new_array = "Style"
+end
+
+def last_element(array)
+  @my_new_array = "Out of The Woods"
+end
+
+def first_element_with_array_methods(array)
+  @my_new_array = "Thailand"
+end
+
+def last_element_with_array_methods(array)
+  @my_new_array = "Myanmar"
+end
+
+def length_of_array(array)
+  @my_new_array = array.length 
+end
