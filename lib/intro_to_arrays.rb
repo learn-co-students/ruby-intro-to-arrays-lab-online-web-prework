1a70e881e3ea9ec45e7a0b2e02e3fd35bc1c2295
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-05 23:07:07 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+ my_new_array = []
+end
+ 
+ def array_with_two_elements
+   my_two_array = ["element1", "element2"]
+ end
+ 
+ def first_element(my_new_array)
+   my_new_array[0]
+ end
+ 
+ def third_element(my_new_array)
+   my_new_array[2]
+ end
+  
+def last_element(my_new_array)
+  my_new_array[-1]
+end
+
+def first_element_with_array_methods(my_new_array)
+  my_new_array.first
+end  
+  
+def last_element_with_array_methods(my_new_array)
+  my_new_array.last
+end
+
+def length_of_array(my_new_array)
+  my_new_array.length
+end
