31775da9e103ae7e791d4050bf09cdbdf7992d73
Kat Maldon <katharinamaldon@gmail.com>
2018-12-07 17:29:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+ def instantiate_new_array
+  @my_new_array = []
+ end
+ 
+ def array_with_two_elements
+   @my_two_array = ["element one", "element two"]
+ end
+
+ def first_element(element)
+   @taylor_swift[0]
+ end
+   
+ def third_element(element)
+   @taylor_swift[2]
+ end
+ 
+ def last_element(element)
+   @taylor_swift[-1]
+ end
+ 
+ def first_element_with_array_methods(element)
+   @south_east_asia.first 
+ end
+ 
+ def last_element_with_array_methods(element)
+   @south_east_asia.last 
+ end
+ 
+ def length_of_array(element)
+   @programming_languages.count
+ end
