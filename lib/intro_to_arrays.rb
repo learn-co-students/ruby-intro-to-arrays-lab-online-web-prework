6248ba9d2dfa7ee6eb9104b3085dacc6407c3b99
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:44:45 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array = ["Westley", "Vivi"]
+end
+
+def first_element(array)
+array =  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+array[0]
+end
+
+def third_element(array)
+  array =  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[2]
+end
+
+def last_element(array)
+  array =  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array[3]
+end
+
+def first_element_with_array_methods(array)
+  array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.last
+end
+
+def length_of_array(array)
+  array = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+array.count
+end
+
