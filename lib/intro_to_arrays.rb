8b8d06a18baf2d90dc08025de860135888b2a670
Kylla Harris <kylla.harris@gmail.com>
2019-01-10 05:16:07 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+[ ]
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+def first_element(array)
+  return array[0]
+  # argument pass into a data method
+  # define a variable for arguments
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  array[-1]
+  
+end
+
+def first_element_with_array_methods(array)
+  array.first
+  
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_array(array)
+  array.length
+end
