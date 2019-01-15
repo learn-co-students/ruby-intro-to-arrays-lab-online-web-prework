393fc9ff40153a1d5e36c86453bf7ac8fb669fbb
Kyle Rosipko <krosipko@outlook.com>
2019-01-15 06:25:20 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["cat", "dog"]
+end
+
+def first_element(array_name)
+  array_name[0]
+end
+
+def third_element(array_name)
+  array_name[2]
+end
+
+def last_element(array_name)
+  array_name[-1]
+end
+
+def first_element_with_array_methods(array_name)
+  array_name.first
+end
+
+def last_element_with_array_methods(array_name)
+  array_name.last
+end
+
+def length_of_array(array_name)
+  array_name.length
+end
