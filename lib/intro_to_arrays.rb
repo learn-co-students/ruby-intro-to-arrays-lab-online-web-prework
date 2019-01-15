589ff7f6305be00c7c82bfcc0b032aefb12e7ff9
Lu Daley <daleyluis@gmail.com>
2019-01-15 05:20:35 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+def instantiate_new_array
+my_new_array = [] 
+end 
+
+def array_with_two_elements
+my_new_array = ["the ole","one-two"] 
+end
+
+def first_element(taylor_swift)
+ #taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+ taylor_swift[0]
+end
+
+def third_element(taylor_swift)
+ taylor_swift[2]
+end
+
+def last_element(taylor_swift)
+  taylor_swift[-1]
+end
+
+def 
+  first_element_with_array_methods(south_east_asia)
+  south_east_asia[0]
+end
+
+def 
+  last_element_with_array_methods(south_east_asia)
+  south_east_asia[3]
+end
+
+def 
+  length_of_array(programming_languages)
+  programming_languages.length
+end
