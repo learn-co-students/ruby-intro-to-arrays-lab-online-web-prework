8fe0dfbd40f1ce302597c57bba419451c69ff878
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-25 16:10:00 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  @my_new_array = Array.new
+end
+
+def array_with_two_elements
+  @my_two_array = Array.new(2)
+end
+
+def first_element(a)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift.first
+end
+
+def third_element(a)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift[2]
+end
+
+def last_element(a)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift.last
+end
+
+def first_element_with_array_methods(a)
+    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    @south_east_asia.first
+end
+
+def last_element_with_array_methods(a)
+    @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    @south_east_asia.last
+end
+
+def length_of_array(a)
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  @programming_languages.length
+end
