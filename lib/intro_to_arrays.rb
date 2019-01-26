770d5cd0ef6751d235edcfbddfe84c22bf9ce9d6
Igor Rud <rud.igor23@gmail.com>
2019-01-26 23:38:38 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  @my_new_array = ["dog", "cat", "Tom", "Jerry"]
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_new_array = ["dog", "cat"]
+end
+
+def first_element(my_first_element)
+  @taylor_swift [0]
+end
+
+def third_element(my_third_element)
+  @taylor_swift[2]
+end
+
+def last_element(my_last_element)
+  @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(first_element)
+  @south_east_asia.first
+end
+
+def last_element_with_array_methods(last_element)
+  @south_east_asia.last
+end
+
+def length_of_array(length)
+  ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"].count
+end
