5c9da02ea3d1c42453e04010e075965075d11b77
declarative-spooling-5152 <kerneltux0@gmail.com>
2019-01-30 15:42:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements
+  array = ["laptop", "TV"]
+end
+
+def first_element(element)
+  my_first_array = ["Welcome to New York", "TV"]
+  my_first_array.first
+end
+
+def third_element(element)
+  my_first_array = ["Welcome to New York", "TV", "Style", "fourth"]
+  my_first_array[2]
+end
+
+def last_element(element)
+  my_first_array = ["Welcome to New York", "TV", "Style", "Out of The Woods"]
+  my_first_array[-1]
+end
+
+def first_element_with_array_methods(array)
+  my_first_array = ["Thailand", "TV", "Style", "Out of The Woods"]
+  my_first_array.first
+end
+
+def last_element_with_array_methods(array)
+  my_first_array = ["Thailand", "TV", "Style", "Myanmar"]
+  my_first_array.last
+end
+
+def length_of_array(array)
+  my_first_array = ["Thailand", "TV", "UK", "Darth Vader", "4th Doctor", "TARDIS", "Style", "Myanmar"]
+  my_first_array.length
+end
