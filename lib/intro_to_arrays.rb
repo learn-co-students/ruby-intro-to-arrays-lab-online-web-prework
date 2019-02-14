763781df3929bf9bfb1c27a6835224934f612f87
alireza nadiri <alireza_nadiri70@yahoo.com>
2019-02-14 19:13:22 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,30 @@
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @my_new_array = [1,2]
+end
+
+def first_element (my_first_element)
+  my_first_element = "Welcome to New York"
+end
+
+def third_element (my_third_element)
+  my_third_element = "Style"
+end
+
+def last_element (my_last_element)
+  my_last_element = "Out of The Woods"
+end
+
+def first_element_with_array_methods (first_country)
+  first_country = "Thailand"
+end
+def last_element_with_array_methods (last_country)
+  last_country = "Myanmar"
+end
+
+def length_of_array (length)
+  length = 8
+end
