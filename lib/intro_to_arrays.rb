e35d1e83fc49220a50ed481c8224c86258a47276
Natasha Chernis <natashachernis@gmail.com>
2019-02-16 06:02:23 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  fruits = []
+end
+
+def array_with_two_elements
+  fruits = ["orange", "apple"]
+end
+
+def first_element(a_cool_array)
+  a_cool_array[0]
+end
+
+def third_element(a_cool_array)
+  a_cool_array[2]
+end
+
+def last_element(a_cool_array)
+  a_cool_array[-1]
+end
+
+def first_element_with_array_methods(a_neat_array)
+  a_neat_array.shift
+end
+
+def last_element_with_array_methods(a_neat_array)
+  a_neat_array.pop
+end
+
+def length_of_array(a_long_array)
+  a_long_array.length
+end
