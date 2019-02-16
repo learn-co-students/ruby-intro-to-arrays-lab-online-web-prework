4362ed565e9858959fa95b5c8c122d787ae1a068
David Stewart <davidstewart@gmail.com>
2019-02-16 15:45:06 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+
+def instantiate_new_array 
+  array = []
+end
+
+def array_with_two_elements
+  array = ["Hello","bye"]
+end
+
+def first_element(x)
+  x.first
+end
+
+def third_element(x)
+  x[2]
+end
+
+def last_element(x)
+    x.last
+end
+
+def first_element_with_array_methods(x)
+  x.first
+  
+end
+
+def last_element_with_array_methods(x)
+  x.last
+end
+
+def length_of_array(x)
+  x.length
+  
+end
