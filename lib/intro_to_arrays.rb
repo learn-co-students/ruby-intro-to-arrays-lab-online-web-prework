fff521e463509e55c988e898459682390c296125
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-03-02 16:07:31 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  instantiate_new_array =  ["Array"]
+end
+
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["array", "world"]
+end
+
+def first_element(arg)
+  first_element = "Welcome to New York"
+end
+
+def third_element(arg)
+  third_element = "Style"
+end
+
+def last_element(arg)
+  last_element = "Out of The Woods"
+end
+  
+def first_element_with_array_methods(arg)
+  first_element_with_array_methods = "Thailand"
+end
+
+def last_element_with_array_methods(arg)
+  last_element_with_array_methods = "Myanmar"
+end
+  
+def length_of_array(arg)
+  length_of_array = 8
+end
+
