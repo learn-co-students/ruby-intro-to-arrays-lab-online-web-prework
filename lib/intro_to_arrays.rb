c977cd9e7eb6e1ab0464e92eb5d8789170002b5d
Patrick A <akolo@hotmail.com>
2019-03-03 19:16:49 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,70 +1,38 @@
-def @my_new_array
-	@my_new_array = instantiate_new_array
-end
-
-def @my_new_array
+def instantiate_new_array
 	@my_new_array = []
 end 
 
-def @my_new_array
-	size = @my_new_array.size
-end 
-
-def @my_new_array
+def array_with_two_elements
 	@my_new_array = ["cars", "bikes"]
-	puts @my_new_array.ispect
 end 
 
-def @my_new_array
-	size = @my_new_array.size
+def first_element (my_first_element)
+	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = @taylor_swift.first
+end
+
+def third_element(my_third_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = @taylor_swift[2]
 end 
 
-def @taylor_swift 
-	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+def last_element(my_last_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = @taylor_swift.last
 end 
 
-def @taylor_swift 
-	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-	my_first_element = @taylor_swift.first
-end
-
-def @taylor_swift 
-	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-	my_third_element = @taylor_swift.third
-end
-
-
-def @taylor_swift 
-	@taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
-	my_last_element = @taylor_swift.last
-end
-
-
-def @south_east_asia  
-	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	my_last_element = @south_east_asia.last
-end
-
-def @south_east_asia  
+def first_element_with_array_methods(first_country)   
 	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 	first_country = @south_east_asia.first
 end
 
-def @south_east_asia  
+def last_element_with_array_methods(my_last_country)   
 	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	first_country = @south_east_asia.first
+	my_last_country = @south_east_asia.last
 end
 
-def @south_east_asia  
-	@south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-	last_country = @south_east_asia.last
-end
 
-def @programming_languages  
+def length_of_array(length)  
 	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-end
-
-def @programming_languages  
-	@programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-	lenght = @programming_languages.size
+	length = @programming_languages.size
 end
