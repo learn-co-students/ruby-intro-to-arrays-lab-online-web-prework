05bc76958ae1e92ef44f5712fc18016895ea98bb
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-03-28 15:38:56 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  da_bros = ["Justin", "Joseph"]
+end
+
+def first_element(array)
+my_new_array = "Welcome to New York"
+end
+
+def third_element(array)
+  my_new_array = "Style"
+
+end
+
+def last_element(array)
+  my_new_array = "Out of The Woods"
+  
+end
+
+def first_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.first
+  
+end
+
+def last_element_with_array_methods(south_east_asia)
+  south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  south_east_asia.last 
+end
+
+def length_of_array(programming_languages)
+  programming_languages.length
+  
+end
