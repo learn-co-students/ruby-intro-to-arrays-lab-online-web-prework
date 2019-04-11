1abd3179b02759fba1cae04742beb9ce9b7f71dd
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-11 23:29:12 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+  Array[ ]
+end  
+
+def array_with_two_elements
+  Array["thing1", "thing2"]
+end 
+
+def first_element(array)
+  array = taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  array.first
+end  
+
+def third_element(array)
+  array[2]
+end  
+
+def last_element(array)
+  array[-1]
+end  
+
+def my_new_array(array)
+  array = instantiate_new_array
+end  
+
+def first_element_with_array_methods(array)  
+  array = south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  array.first
+end  
+  
+def last_element_with_array_methods(array)
+  array.last
+end  
+
+def length_of_array(array)
+  array.length
+end  
+  
+  
+  
