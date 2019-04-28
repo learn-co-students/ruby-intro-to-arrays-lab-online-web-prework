ef61abd895d2e0a876625810b6f6fd1a854d3a92
Malary Robinson <robinmal16@gmail.com>
2019-04-28 21:59:41 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array = ["Scamp", "Pepper"]
+end
+
+def first_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
+  taylor_swift[0]
+end
+
+def third_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
+  taylor_swift[2]
+end
+
+def last_element(taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"])
+  taylor_swift[3]
+end
+
+def first_element_with_array_methods(south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"])
+  south_east_asia.first
+end
+
+def last_element_with_array_methods(south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"])
+  south_east_asia.last
+end
+
+def length_of_array(programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"])
+  programming_languages.length
+end
