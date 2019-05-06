0db3f09f38158d454d16b2ecb0a640bff4372ad9
Jasmine Chen <jasmine1226@gmail.com>
2019-05-06 03:30:00 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array = ["a", "b"]
+end
+
+def first_element(array)
+  my_first_element = array.first
+end
+
+def third_element(array)
+  my_third_element = array[2]
+end
+
+def last_element(array)
+  my_last_element = array.last
+end
+
+def first_element_with_array_methods(array)
+  my_first_element = array.shift
+end
+
+def last_element_with_array_methods(array)
+  my_last_element = array.pop
+end
+
+def length_of_array(array)
+  length_of_array = array.length
+end
