c3138d01aa4186401a8a63e59234d4dff93c39bb
johncban <johncban@gmail.com>
2019-05-10 22:35:42 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  Array.new
+end
+
+def array_with_two_elements
+  ar = [1, 2]
+end
+
+def first_element(ar)
+  ar[0]
+end
+
+def third_element(ar)
+  ar[2]
+end
+
+def last_element(ar)
+  ar[-1]
+end
+
+def first_element_with_array_methods(sea)
+  sea.first
+end
+
+def last_element_with_array_methods(sea)
+  sea.last
+end
+
+def length_of_array(pl)
+  pl.length
+end
