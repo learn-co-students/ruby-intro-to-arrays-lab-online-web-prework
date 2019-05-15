d84a4ee5059ade577ed4979a79206147006e4bf8
Angela Lo <angelawenlo@gmail.com>
2019-05-15 15:26:53 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,33 +1,33 @@
 def instantiate_new_array
-  instantiate_new_array=[]
-  @my_array=instantiate_new_array
+  new_array =[]
+  new_array
 end
 
 def array_with_two_elements
-  array_with_two_elements = [1,2]
-  @my_array=array_with_two_elements
+  new_array = [1,2]
+  new_array
 end
 
 def first_element(my_first_element)
-  my_first_element = @taylor_swift[0] 
+  my_first_element[0]
 end
 
 def third_element(my_third_element)
-  my_third_element = @taylor_swift[2]
+  my_third_element[2]
 end
 
 def last_element(my_last_element)
-  my_last_element = @taylor_swift[-1]
+  my_last_element[-1]
 end
 
 def first_element_with_array_methods(first_country)
-  first_country = @south_east_asia.first
-end
-    
-def last_element_with_array_methods(last_country)
-  last_country = @south_east_asia.last
+  first_country.first
 end
 
-def length_of_array(length)
- length = @programming_languages.length
+def last_element_with_array_methods(last_country)
+  last_country.last
+end
+
+def length_of_array(array)
+ array.length
 end
