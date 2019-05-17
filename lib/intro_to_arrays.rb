6b969fa5ce1e18172f4db4203a99fa662e01d089
layla dantas <laylatdantas@gmail.com>
2019-05-17 23:44:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+   @Array = ["Array"]
+   end
+
+def instantiate_new_array
+   @Array = []
+ end 
+  
+def array_with_two_elements
+  @Array = [1, 2]
+end 
+
+def first_element(my_first_element)
+  @taylor_swift[0]
+end
+
+def third_element(my_third_element)
+  @taylor_swift[2]
+end 
+
+def last_element(my_last_element)
+  @taylor_swift[-1]
+end
+  
+  def first_element_with_array_methods(first_country)
+    @south_east_asia[0]
+  end
+  
+  def last_element_with_array_methods(last_country)
+    @south_east_asia[-1]
+  end 
+  
+  def length_of_array(length)
+    @programming_languages.length
+  end
+  
+  
+  
+  
+  
