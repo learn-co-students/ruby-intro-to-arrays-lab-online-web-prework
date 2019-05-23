2450e7c0ca0f8c92d28ba9d665f3883fb00339af
Amanda King <amapickell@gmail.com>
2019-05-23 15:32:37 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+#instantiating
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+#indexing
+def first_element (array)
+  array[0]
+end
+
+def third_element (array)
+  array[2]
+end
+
+def last_element (array)
+  array[-1]
+end
+
+#ruby methods to return values
+def first_element_with_array_methods (array)
+  array.first
+end
+
+def last_element_with_array_methods (array)
+  array.last
+end
+
+#ruby methods to get information
+def length_of_array (array)
+  array.length
+end
